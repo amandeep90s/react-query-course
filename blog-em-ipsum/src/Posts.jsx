4beef22e5ff0c68307e93ed72c5ffdbcf418cfe2
Amandeep Singh <programmeramandeep@gmail.com>
2025-08-15 11:18:00 +0530

    feat: Handling loading and error states

--- blog-em-ipsum/src/Posts.jsx
+++ blog-em-ipsum/src/Posts.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
+import { useQuery } from "@tanstack/react-query";
 import { PostDetail } from "./PostDetail";
+import { fetchPosts } from "./api";
+
 const maxPostPage = 10;
 
 export function Posts() {
@@ -8,7 +11,20 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Error loading posts </h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
