4d913ac8a4bec73f3aa77eb5541f8ad055c1152e
Amandeep Singh <programmeramandeep@gmail.com>
2025-08-15 13:36:32 +0530

    feat: Query Keys

--- blog-em-ipsum/src/PostDetail.jsx
+++ blog-em-ipsum/src/PostDetail.jsx
@@ -1,8 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import "./PostDetail.css";
+import { fetchComments } from "./api";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) return <h3>Loading comments...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Error loading comments</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
