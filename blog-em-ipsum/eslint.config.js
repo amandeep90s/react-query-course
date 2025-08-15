2f6c162130aaf241437dc4a35c3dbdefe888730b
Amandeep Singh <programmeramandeep@gmail.com>
2025-08-15 13:46:12 +0530

    feat: Refactor ESLint configuration to use array format and include additional plugins

--- blog-em-ipsum/eslint.config.js
+++ blog-em-ipsum/eslint.config.js
@@ -1,30 +1,35 @@
 import js from "@eslint/js";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      "plugin:@tanstack/eslint-plugin-query/recommended",
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
-    },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-    },
-  },
-]);
+export default [
+	{
+		ignores: ["dist"],
+	},
+	{
+		files: ["**/*.{js,jsx}"],
+		...js.configs.recommended,
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				ecmaFeatures: { jsx: true },
+				sourceType: "module",
+			},
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+			"@tanstack/query": tanstackQuery,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+			...tanstackQuery.configs.recommended.rules,
+			"no-unused-vars": ["warn", { varsIgnorePattern: "^[A-Z_]" }],
+		},
+	},
+];
